e4bea0c2e307bccaf72d0e64cfc4910b74cc2eb8
Nandan <devadula.nandan@gmail.com>
2022-05-18 23:53:22 +0530

    dynamic timer

--- src/Todo.js
+++ src/Todo.js
@@ -3,8 +3,31 @@ import React, { useContext, useEffect, useState } from "react";
 import AddBar from "./AddBar";
 
 const Card = (props) => {
+  const d = new Date(props.todo.deadline + "+0530");
+  const [deadline, setDeadline] = useState(
+    new Date(props.todo.deadline + "+0530")
+  );
+  // decrement time every second to show time left
+  useEffect(() => {
+    const timer = setInterval(() => {
+      d.setSeconds(d.getSeconds() - 1);
+      setDeadline(d);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [deadline]);
   return (
-    <div className={"break-inside p-3 rounded shadow-lg mb-5 " + (props.todo.priority === 0 ? "bg-gray-200" : props.todo.priority === 1 ? "bg-yellow-300" : props.todo.priority === 2 ? "bg-orange-300" : "bg-red-300")}>
+    <div
+      className={
+        "break-inside p-3 rounded shadow-lg mb-5 " +
+        (props.todo.priority === 0
+          ? "bg-gray-200"
+          : props.todo.priority === 1
+          ? "bg-yellow-300"
+          : props.todo.priority === 2
+          ? "bg-orange-300"
+          : "bg-red-300")
+      }
+    >
       <div className="title-text mb-2">
         <pre className="mb-2 font-bold">{props.todo.title}</pre>
         <div className="">
@@ -13,10 +36,17 @@ const Card = (props) => {
       </div>
       <div className="grid grid-cols-3">
         <div className="col-span-2">
-          <button className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md" title="edit">
+          <button
+            className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
+            title="edit"
+          >
             <i className="fas m-auto fa-pen-square"></i>
           </button>
-          <button onClick={() => props.checkTodo(props.todo.id)} className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md" title="complete">
+          <button
+            onClick={() => props.checkTodo(props.todo.id)}
+            className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
+            title="complete"
+          >
             <i className="fas m-auto fa-check-square"></i>
           </button>
           <button
@@ -35,16 +65,22 @@ const Card = (props) => {
           <div className="flex" title={props.todo.deadline}>
             <p className="text-xs m-auto bg-black/10 px-1 rounded-md py-0.5">
               {
-                // show number of days left, if deadline is less than today then show hours left, if deadline is less than an hour then show minutes left, if deadline is less than a minute then show seconds left
-                new Date(props.todo.deadline) > new Date()
-                  ? Math.floor((new Date(props.todo.deadline) - new Date()) / (1000 * 60 * 60 * 24))
-                    ? `${Math.floor((new Date(props.todo.deadline) - new Date()) / (1000 * 60 * 60 * 24))} days left`
-                    : Math.floor((new Date(props.todo.deadline) - new Date()) / (1000 * 60 * 60))
-                    ? `${Math.floor((new Date(props.todo.deadline) - new Date()) / (1000 * 60 * 60))} hours left`
-                    : Math.floor((new Date(props.todo.deadline) - new Date()) / (1000 * 60))
-                    ? `${Math.floor((new Date(props.todo.deadline) - new Date()) / (1000 * 60))} minutes left`
-                    : `${Math.floor((new Date(props.todo.deadline) - new Date()) / 1000)} seconds left`
-                  : "deadline passed"
+                // show years and months or months and days or days and hours or hours and minutes or minutes and seconds or seconds if the deadline is in future else show overdue
+                deadline.getTime() > new Date().getTime()
+                  ? deadline.getFullYear() === new Date().getFullYear()
+                    ? deadline.getMonth() === new Date().getMonth()
+                      ? deadline.getDate() === new Date().getDate()
+                        ? deadline.getHours() === new Date().getHours()
+                          ? deadline.getMinutes() === new Date().getMinutes()
+                            ? deadline.getSeconds() === new Date().getSeconds()
+                              ? "now"
+                              : `${deadline.getSeconds() - new Date().getSeconds()}s`
+                            : `${deadline.getMinutes() - new Date().getMinutes()}m ${deadline.getSeconds() - new Date().getSeconds()}s`
+                          : `${deadline.getHours() - new Date().getHours()}h ${deadline.getMinutes() - new Date().getMinutes()}m`
+                        : `${deadline.getDate() - new Date().getDate()}d ${deadline.getHours() - new Date().getHours()}h`
+                      : `${deadline.getMonth() - new Date().getMonth()}m ${deadline.getDate() - new Date().getDate()}d`
+                    : `${deadline.getFullYear() - new Date().getFullYear()}y ${deadline.getMonth() - new Date().getMonth()}m`
+                  : "overdue"
               }
             </p>
           </div>
