931f3ad2e8316e3cd7b1a6571965ddca33bf0625
Nandan <devadula.nandan@gmail.com>
2022-05-22 15:41:56 +0530

    dty

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Welcome() {
   return (
-    <div className="flex flex-col h-[calc(100vh-76px)] align-middle justify-evenly">
+    <div className="flex flex-col h-[calc(100vh-76px)] align-middle justify-evenly px-4">
       <h1 className=" text-teal-700 text-4xl font-extrabold text-center">Welcome to my Todo app</h1>
       <div className="flex align-middle justify-center">
         <button className="text-teal-800 rounded-3xl mr-3 bg-teal-300 hover:bg-teal-400 hover:shadow-2xl transition-all px-4 py-2" onClick={() => (window.location.href = "/signup")}>
