3629fc5ff27c0b0aa73d4475d81be66653f2dcda
Nandan <devadula.nandan@gmail.com>
2022-05-29 11:19:46 +0530

    yes

--- src/Todo.js
+++ src/Todo.js
@@ -18,142 +18,148 @@ const Card = (props) => {
     return () => clearInterval(timer);
   }, [deadline]);
   return (
-    <div
-      className={
-        "break-inside-avoid p-3 rounded shadow mb-5 " +
-        (props.todo.priority === 0
-          ? "bg-gray-100"
-          : props.todo.priority === 1
-          ? "bg-yellow-300"
-          : props.todo.priority === 2
-          ? "bg-orange-300"
-          : props.todo.priority === 3
-          ? "bg-red-300"
-          : "bg-green-300")
-      }
-    >
-      <div className="title-text mb-2">
-        <pre className="whitespace-pre-wrap mb-2 font-bold text-lg cursor-default">
-          {props.todo.title}
-        </pre>
-        <div className="">
-          <pre className="whitespace-pre-wrap cursor-default">
-            {props.todo.text}
+    <div className="break-inside-avoid p-1.5">
+      <div
+        className={
+          "p-3 rounded shadow " +
+          (props.todo.priority === 0
+            ? "bg-gray-100"
+            : props.todo.priority === 1
+            ? "bg-yellow-300"
+            : props.todo.priority === 2
+            ? "bg-orange-300"
+            : props.todo.priority === 3
+            ? "bg-red-300"
+            : "bg-green-300")
+        }
+      >
+        <div className="title-text mb-2">
+          <pre className="whitespace-pre-wrap mb-2 font-bold text-lg cursor-default">
+            {props.todo.title}
           </pre>
+          <div className="">
+            <pre className="whitespace-pre-wrap cursor-default">
+              {props.todo.text}
+            </pre>
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-3">
-        <div className="col-span-2 -ml-2 -mb-2">
-          {props.todo.priority >= 0 && (
-            <>
+        <div className="grid grid-cols-3">
+          <div className="col-span-2 -ml-2 -mb-2">
+            {props.todo.priority >= 0 && (
+              <>
+                <button
+                  className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
+                  title="edit"
+                  onClick={() => {
+                    props.setShowModal(true);
+                    props.setEditData({
+                      title: props.todo.title,
+                      text: props.todo.text,
+                      priority: props.todo.priority,
+                      deadline: new Date(props.todo.deadline)
+                        .toISOString()
+                        .slice(0, 16),
+                      id: props.todo.id,
+                    });
+                  }}
+                >
+                  <i className="fas m-auto fa-pen-square"></i>
+                </button>
+                <button
+                  onClick={() => props.checkTodo(props.todo.id)}
+                  className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
+                  title="complete"
+                >
+                  <i className="fas m-auto fa-check-square"></i>
+                </button>
+              </>
+            )}
+            {props.todo.priority < 0 && (
               <button
-                className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
-                title="edit"
                 onClick={() => {
-                  props.setShowModal(true);
-                  props.setEditData({
-                    title: props.todo.title,
-                    text: props.todo.text,
-                    priority: props.todo.priority,
-                    deadline: new Date(props.todo.deadline)
-                      .toISOString()
-                      .slice(0, 16),
-                    id: props.todo.id,
-                  });
+                  axios
+                    .post(
+                      "https://nandan1996-todo-flask-api.herokuapp.com/uncheck.todo",
+                      {
+                        id: props.todo.id,
+                      }
+                    )
+                    .then((res) => {
+                      props.setTodos(
+                        props.todos.filter((todo) => todo.id !== props.todo.id)
+                      );
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
                 }}
-              >
-                <i className="fas m-auto fa-pen-square"></i>
-              </button>
-              <button
-                onClick={() => props.checkTodo(props.todo.id)}
                 className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
-                title="complete"
+                title="restore"
               >
                 <i className="fas m-auto fa-check-square"></i>
               </button>
-            </>
-          )}
-          {props.todo.priority < 0 && (
+            )}
             <button
-              onClick={() => {
-                axios
-                  .post(
-                    "https://nandan1996-todo-flask-api.herokuapp.com/uncheck.todo",
-                    {
-                      id: props.todo.id,
-                    }
-                  )
-                  .then((res) => {
-                    props.setTodos( props.todos.filter(todo => todo.id !== props.todo.id));
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                  });
-              }}
               className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
-              title="restore"
+              onClick={() => {
+                // msnry.remove(this.parent().parent());
+                props.removeTodo(props.todo.id);
+              }}
+              title="delete"
             >
-              <i className="fas m-auto fa-check-square"></i>
+              <i className="fas m-auto fa-times-circle"></i>
             </button>
-          )}
-          <button
-            className="p-2 h-9 w-9 inline-flex rounded-full transition-all active:bg-black/20 hover:bg-black/10 hover:shadow-md"
-            onClick={() => {
-              // msnry.remove(this.parent().parent());
-              props.removeTodo(props.todo.id);
-            }}
-            title="delete"
-          >
-            <i className="fas m-auto fa-times-circle"></i>
-          </button>
-        </div>
+          </div>
 
-        {props.todo.deadline && (
-          <div className="flex" title={props.todo.deadline}>
-            <p className="text-xs mt-auto ml-auto bg-black/10 px-1 rounded-md py-0.5 hover:bg-black/20 cursor-default">
-              {
-                // show years and months or months and days or days and hours or hours and minutes or minutes and seconds or seconds if the deadline is in future else show overdue
-                deadline
-                  ? deadline.getTime() > new Date().getTime()
-                    ? deadline.getFullYear() === new Date().getFullYear()
-                      ? deadline.getMonth() === new Date().getMonth()
-                        ? deadline.getDate() === new Date().getDate()
-                          ? deadline.getHours() === new Date().getHours()
-                            ? deadline.getMinutes() === new Date().getMinutes()
-                              ? deadline.getSeconds() ===
-                                new Date().getSeconds()
-                                ? "now"
+          {props.todo.deadline && (
+            <div className="flex" title={props.todo.deadline}>
+              <p className="text-xs mt-auto ml-auto bg-black/10 px-1 rounded-md py-0.5 hover:bg-black/20 cursor-default">
+                {
+                  // show years and months or months and days or days and hours or hours and minutes or minutes and seconds or seconds if the deadline is in future else show overdue
+                  deadline
+                    ? deadline.getTime() > new Date().getTime()
+                      ? deadline.getFullYear() === new Date().getFullYear()
+                        ? deadline.getMonth() === new Date().getMonth()
+                          ? deadline.getDate() === new Date().getDate()
+                            ? deadline.getHours() === new Date().getHours()
+                              ? deadline.getMinutes() ===
+                                new Date().getMinutes()
+                                ? deadline.getSeconds() ===
+                                  new Date().getSeconds()
+                                  ? "now"
+                                  : `${
+                                      deadline.getSeconds() -
+                                      new Date().getSeconds()
+                                    }s`
                                 : `${
+                                    deadline.getMinutes() -
+                                    new Date().getMinutes()
+                                  }m ${
                                     deadline.getSeconds() -
                                     new Date().getSeconds()
                                   }s`
                               : `${
+                                  deadline.getHours() - new Date().getHours()
+                                }h ${
                                   deadline.getMinutes() -
                                   new Date().getMinutes()
-                                }m ${
-                                  deadline.getSeconds() -
-                                  new Date().getSeconds()
-                                }s`
-                            : `${
+                                }m`
+                            : `${deadline.getDate() - new Date().getDate()}d ${
                                 deadline.getHours() - new Date().getHours()
-                              }h ${
-                                deadline.getMinutes() - new Date().getMinutes()
-                              }m`
-                          : `${deadline.getDate() - new Date().getDate()}d ${
-                              deadline.getHours() - new Date().getHours()
-                            }h`
-                        : `${deadline.getMonth() - new Date().getMonth()}m ${
-                            deadline.getDate() - new Date().getDate()
-                          }d`
-                      : `${
-                          deadline.getFullYear() - new Date().getFullYear()
-                        }y ${deadline.getMonth() - new Date().getMonth()}m`
-                    : "overdue"
-                  : "loading..."
-              }
-            </p>
-          </div>
-        )}
+                              }h`
+                          : `${deadline.getMonth() - new Date().getMonth()}m ${
+                              deadline.getDate() - new Date().getDate()
+                            }d`
+                        : `${
+                            deadline.getFullYear() - new Date().getFullYear()
+                          }y ${deadline.getMonth() - new Date().getMonth()}m`
+                      : "overdue"
+                    : "loading..."
+                }
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
@@ -162,7 +168,7 @@ const Card = (props) => {
 export default function TodoList(props) {
   const isLogged = props.isLogged;
   const [todos, setTodos] = useState([]);
-  const [activeTab , setActiveTab] = useState("all");
+  const [activeTab, setActiveTab] = useState("all");
   const [editData, setEditData] = useState({});
   const [showModal, setShowModal] = useState(false);
   useEffect(() => {
@@ -216,8 +222,8 @@ export default function TodoList(props) {
   return (
     <div className="md:container md:mx-auto">
       <AddBar setTodos={setTodos} todos={todos} activeTab={activeTab} />
-      <div className="md:container md:mx-auto px-3 sm:px-7 lg:px-8">
-        <div className="flex">
+      <div className="md:container md:mx-auto sm:px-4 lg:px-5">
+        <div className="flex px-3">
           <ul className="flex mb-2 rounded-lg shadow">
             <li className="flex">
               <input
@@ -232,7 +238,7 @@ export default function TodoList(props) {
                 className="px-3 py-2 bg-gray-100 rounded-l-lg cursor-pointer font-semibold text-gray-600 focus:outline-none hover:bg-gray-200 peer-checked:bg-teal-400"
                 for="all"
                 onClick={() => {
-                  setActiveTab('all');
+                  setActiveTab("all");
                   getTodos({
                     active: true,
                   });
@@ -356,7 +362,7 @@ export default function TodoList(props) {
           </ul>
         </div>
 
-        <div className="md:container md:mx-auto columns-1 sm:columns-2 lg:columns-3 2xl:columns-4 gap-5 pb-5">
+        <div className="md:container md:mx-auto columns-1 sm:columns-2 lg:columns-3 2xl:columns-4 gap-0 px-1.5 pb-5">
           <>
             {showModal ? (
               <>
